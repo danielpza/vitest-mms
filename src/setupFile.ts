b2b45f2082ce729bf7ec52e20c5f95c0f8df386b
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-21 15:51:07 -0400

    add comment

--- src/setupFile.ts
+++ src/setupFile.ts
@@ -2,6 +2,7 @@ import { Db, MongoClient } from "mongodb";
 import { afterAll, beforeAll, beforeEach, inject } from "vitest";
 // hack to keep imported vitest types
 export type { TestContext } from "vitest";
+// hack to fix pnpm build issue
 export type {} from "./globalSetup.js";
 
 import { randomUUID } from "node:crypto";
