62a483cc693bb3bff4076fd033ff670a8c83b751
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-21 14:52:10 -0400

    update

--- src/setupFile.ts
+++ src/setupFile.ts
@@ -1,6 +1,4 @@
-import { randomUUID } from "node:crypto";
-
-import { Db, MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
 import { afterAll, beforeAll, beforeEach, inject } from "vitest";
 
 import type {} from "./globalSetup.js";
@@ -9,7 +7,6 @@ export const globalContext = Symbol("vitest-mms");
 
 interface GlobalContext {
   mongoClient: MongoClient;
-  db: Db;
 }
 
 beforeAll(async () => {
@@ -20,7 +17,6 @@ beforeAll(async () => {
 
   const context: GlobalContext = {
     mongoClient: mongoClient,
-    db: mongoClient.db(randomUUID()),
   };
 
   // @ts-expect-error How to defined a symbol in globalThis?
@@ -36,7 +32,6 @@ afterAll(async () => {
 declare module "vitest" {
   interface TestContext {
     mongoClient: MongoClient;
-    db: Db;
   }
 }
 
--- tests/global-setup/src/index.test.ts
+++ tests/global-setup/src/index.test.ts
@@ -2,18 +2,20 @@ import { expect, test } from "vitest";
 
 import { insertUser } from "./index.js";
 
-test("mmsTest", async ({ mongoClient, db }) => {
+test("mmsTest", async ({ mongoClient }) => {
+  const db = mongoClient.db("test1");
   expect(mongoClient).toBeDefined();
-  expect(db).toBeDefined();
 });
 
-test("check dbs are unique1", async ({ db }) => {
+test("check dbs are unique1", async ({ mongoClient }) => {
+  const db = mongoClient.db("test2");
   await insertUser(db);
   await insertUser(db);
   expect(await db.collection("users").countDocuments()).toBe(2);
 });
 
-test("check dbs are unique2", async ({ db }) => {
+test("check dbs are unique2", async ({ mongoClient }) => {
+  const db = mongoClient.db("test3");
   await insertUser(db);
   expect(await db.collection("users").countDocuments()).toBe(1);
 });
