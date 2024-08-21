065368f192305889a50bf068b659475804899d2e
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-21 14:59:50 -0400

    fix

--- src/setupFile.ts
+++ src/setupFile.ts
@@ -5,7 +5,7 @@ import { afterAll, beforeAll, beforeEach, inject } from "vitest";
 import type {} from "./globalSetup.js";
 import { randomUUID } from "node:crypto";
 
-export const globalContext = Symbol("vitest-mms");
+export const globalContextSymbol = Symbol("vitest-mms");
 
 interface GlobalContext {
   mongoClient: MongoClient;
@@ -29,18 +29,18 @@ beforeAll(async () => {
   };
 
   // @ts-expect-error How to defined a symbol in globalThis?
-  globalThis[globalContext] = context;
+  globalThis[globalContextSymbol] = context;
 });
 
 afterAll(async () => {
   // @ts-expect-error How to defined a symbol in globalThis?
-  const context = globalThis[globalContext] as GlobalContext;
+  const context = globalThis[globalContextSymbol] as GlobalContext;
   await context.mongoClient.close();
 });
 
 beforeEach(async (context) => {
   // @ts-expect-error How to defined a symbol in globalThis?
-  const globalContext = globalThis[globalContext] as GlobalContext;
+  const globalContext = globalThis[globalContextSymbol] as GlobalContext;
   const { mongoClient } = globalContext;
   const db = mongoClient.db(randomUUID());
   Object.assign(context, { mongoClient, db });
