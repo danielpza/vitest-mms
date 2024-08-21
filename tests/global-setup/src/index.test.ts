c72a730c6816f7e628f038459ab2b1133f760aff
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-21 15:00:02 -0400

    rollback test

--- tests/global-setup/src/index.test.ts
+++ tests/global-setup/src/index.test.ts
@@ -2,20 +2,18 @@ import { expect, test } from "vitest";
 
 import { insertUser } from "./index.js";
 
-test("mmsTest", async ({ mongoClient }) => {
-  const db = mongoClient.db("test1");
+test("mmsTest", async ({ mongoClient, db }) => {
   expect(mongoClient).toBeDefined();
+  expect(db).toBeDefined();
 });
 
-test("check dbs are unique1", async ({ mongoClient }) => {
-  const db = mongoClient.db("test2");
+test("check dbs are unique1", async ({ db }) => {
   await insertUser(db);
   await insertUser(db);
   expect(await db.collection("users").countDocuments()).toBe(2);
 });
 
-test("check dbs are unique2", async ({ mongoClient }) => {
-  const db = mongoClient.db("test3");
+test("check dbs are unique2", async ({ db }) => {
   await insertUser(db);
   expect(await db.collection("users").countDocuments()).toBe(1);
 });
