ea919c514d182b3cd62b7be9578f256d88bba033
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-28 10:06:03 -0400

    Revert "feat: add objectIdEqualityTester helper"
    This reverts commit 95794adeb397c433bd66d20b41275cc667907d39.

--- src/objectIdEqualityTester.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from "mongodb";
-import { expect } from "vitest";
-
-expect.addEqualityTesters([
-  // https://vitest.dev/api/expect.html#expect-addequalitytesters
-  function objectIdEqualityTester(a: unknown, b: unknown) {
-    if (a instanceof ObjectId && b instanceof ObjectId) return a.equals(b);
-  },
-]);
