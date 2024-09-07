8cc2eff84a64f3e3d9e575e6cf081167a77002ec
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-09-07 17:55:41 -0400

    refactor: use global variable instead of modifying the global context

--- src/setupFile.ts
+++ src/setupFile.ts
@@ -7,12 +7,6 @@ export type {} from "./globalSetup.js";
 
 import { randomUUID } from "node:crypto";
 
-export const globalContextSymbol = Symbol("vitest-mms");
-
-interface GlobalContext {
-  mongoClient: MongoClient;
-}
-
 declare module "vitest" {
   interface TestContext {
     mongoClient: MongoClient;
@@ -20,30 +14,20 @@ declare module "vitest" {
   }
 }
 
+let mongoClient: MongoClient;
+
 beforeAll(async () => {
   const uri = inject("MONGO_URI");
 
-  const mongoClient = new MongoClient(uri);
+  mongoClient = new MongoClient(uri);
   await mongoClient.connect();
-
-  const context: GlobalContext = {
-    mongoClient: mongoClient,
-  };
-
-  // @ts-expect-error How to defined a symbol in globalThis?
-  globalThis[globalContextSymbol] = context;
 });
 
 afterAll(async () => {
-  // @ts-expect-error How to defined a symbol in globalThis?
-  const context = globalThis[globalContextSymbol] as GlobalContext;
-  await context.mongoClient.close();
+  await mongoClient.close();
 });
 
 beforeEach(async (context) => {
-  // @ts-expect-error How to defined a symbol in globalThis?
-  const globalContext = globalThis[globalContextSymbol] as GlobalContext;
-  const { mongoClient } = globalContext;
   const db = mongoClient.db(randomUUID());
   Object.assign(context, { mongoClient, db });
 });
