80ced6b4e838c0df4445685248b95baab9e247c8
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-09-08 21:24:42 -0400

    chore!: remove deprecated exports

--- src/_deprecated_mmsTest.ts
+++
@@ -1,4 +0,0 @@
-console.warn(
-  "'vitest-mms/test' is deprecated. Please use 'vitest-mms/mongodb/test' instead.",
-);
-export * from "./mongodb/mmsTest.js";
--- src/_deprecated_setupFile.ts
+++
@@ -1,4 +0,0 @@
-console.warn(
-  "'vitest-mms/setupFile' is deprecated. Please use 'vitest-mms/mongodb/setupFile' instead.",
-);
-export * from "./mongodb/setupFile.js";
