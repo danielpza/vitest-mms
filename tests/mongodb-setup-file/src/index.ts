9bc7698efbd9c9e5acd236e6d84f9bcfd528f412
Daniel Perez Alvarez <danielpza@protonmail.com>
2025-06-25 16:50:13 -0700

    chore!: remove deprecated setupFile entrypoints
    Removed the setupFile entrypoints for mongodb and mongoose. These relied on
    beforeEach/afterEach hooks which are marked as deprecated on vitest.
    
    From the vitest docs:
    
    > This is an outdated way of extending context and it will not work when the test is extended with test.extend.
    
    Reference
    
    - https://vitest.dev/guide/test-context.html#beforeeach-and-aftereach

--- tests/mongodb-setup-file/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Db } from "mongodb";
-
-export async function insertUser(db: Db) {
-  await db.collection("users").insertOne({ name: "John" });
-}
--- tests/mongoose-setup-file/vitest.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    globalSetup: ["vitest-mms/globalSetup"],
-    setupFiles: ["vitest-mms/mongoose/setupFile"],
-  },
-});
